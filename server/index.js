0aa2c1d95c3d99b1a67624e53163ee0c517ff73a
junior <juniorbohry@gmail.com>
2022-10-24 11:22:37 -0300

    alteração rotas api

--- server/index.js
+++ server/index.js
@@ -7,11 +7,6 @@ const path = require('path')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// app.use(
-//     express.urlencoded({        
-//         extended: true
-//     })
-// )
 app.use(express.json())
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
